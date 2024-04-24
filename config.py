a50ce92e76fa741dd3c5927ef2ad2c1238d06fe2
boonhong5512 <94824732+boonhong5512@users.noreply.github.com>
2024-04-24 18:19:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cp4oua0imc0l.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "awsuser123"
+customdb = "employee"
+custombucket = "chuaboonhong-employee"
+customregion = "us-east-1"
+
